461dfba9edc976c89f83c2277fb82ea91ca2f798
Alfredo Tito <tito.alfred@hotmail.com>
2021-01-18 18:30:58 -0300

    Library API - Aprendendo a desenhar uma requisiçaõ utilizando TDD

---
+++ library-api/src/test/java/com/alfredots/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,35 @@
+package com.alfredots.libraryapi.api.resource;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    static String BOOK_API = "/api/hooks";
+    
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createBookTest () {
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar error de validação quando não houver dados suficiente para criação do livro.")
+    public void createInvalidBookTest () {
+
+    }
+}
